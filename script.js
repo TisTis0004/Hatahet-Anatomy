3f9870816e6345f87c39911782c4487ea9967c41
Fares <fareshatahet491@gmail.com>
2024-02-12 08:50:00 +0300

    contact btn final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const contactIcons = document.querySelectorAll(".contact-icon");
 document.addEventListener("DOMContentLoaded", function () {
   const engTitle = document.querySelector(".fade-in-left");
   const arTitle = document.querySelector(".fade-in-right");
@@ -29,12 +30,50 @@ sheetsBtn.addEventListener("click", function () {
 const year = document.querySelector("#year");
 const d = new Date();
 year.innerText = d.getFullYear();
-const contactIcons = document.getElementById("contact-icons");
+const contactIconsBackground = document.getElementById(
+  "contact-icons-background"
+);
 function showContact() {
-  contactIcons.style.transform = "rotateY(0)";
+  contactIconsBackground.style.transform = "rotateY(0)";
+  for (let icon of contactIcons) {
+    icon.style.color = "#002845";
+  }
   setTimeout(() => {
-    contactIcons.style.transformOrigin = "right";
-    contactIcons.style.transform = "rotateY(90deg)";
+    contactIconsBackground.style.transformOrigin = "right";
+    contactIconsBackground.style.transform = "rotateY(90deg)";
+    for (let icon of contactIcons) icon.style.color = "#FFF";
   }, 1800);
-  contactIcons.style.transformOrigin = "left";
+  contactIconsBackground.style.transformOrigin = "left";
 }
+contactIcons[0].addEventListener("mouseenter", () => {
+  contactIcons[0].style.transition = "color 0.5s 0s ease";
+  contactIcons[0].style.color = "#ff0000";
+});
+contactIcons[1].addEventListener("mouseenter", () => {
+  contactIcons[1].style.transition = "color 0.5s 0s ease";
+  contactIcons[1].style.color = "#23a8e9";
+});
+contactIcons[2].addEventListener("mouseenter", () => {
+  contactIcons[2].style.transition = "color 0.5s 0s ease";
+  contactIcons[2].style.color = "#0866ff";
+});
+contactIcons[3].addEventListener("mouseenter", () => {
+  contactIcons[3].style.transition = "color 0.5s 0s ease";
+  contactIcons[3].style.color = "#ffdd00";
+});
+contactIcons[0].addEventListener("mouseleave", () => {
+  contactIcons[0].style.transition = "color 0.5s 0s ease";
+  contactIcons[0].style.color = "#FFF";
+});
+contactIcons[1].addEventListener("mouseleave", () => {
+  contactIcons[1].style.transition = "color 0.5s 0s ease";
+  contactIcons[1].style.color = "#FFF";
+});
+contactIcons[2].addEventListener("mouseleave", () => {
+  contactIcons[2].style.transition = "color 0.5s 0s ease";
+  contactIcons[2].style.color = "#FFF";
+});
+contactIcons[3].addEventListener("mouseleave", () => {
+  contactIcons[3].style.transition = "color 0.5s 0s ease";
+  contactIcons[3].style.color = "#FFF";
+});
