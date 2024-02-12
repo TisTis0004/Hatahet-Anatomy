367826685ecd76e3509abd8708a9378c10a3050a
Fares <fareshatahet491@gmail.com>
2024-02-12 23:32:09 +0300

    contact icons animation color

--- script.js
+++ script.js
@@ -35,45 +35,9 @@ const contactIconsBackground = document.getElementById(
 );
 function showContact() {
   contactIconsBackground.style.transform = "rotateY(0)";
-  // for (let icon of contactIcons) {
-  //   icon.style.color = "#002845";
-  // }
   setTimeout(() => {
     contactIconsBackground.style.transformOrigin = "right";
     contactIconsBackground.style.transform = "rotateY(90deg)";
-    // for (let icon of contactIcons) icon.style.color = "#FFF";
   }, 1100);
   contactIconsBackground.style.transformOrigin = "left";
 }
-// contactIcons[0].addEventListener("mouseenter", () => {
-//   contactIcons[0].style.transition = "color 0.5s 0s ease";
-//   contactIcons[0].style.color = "#ff0000";
-// });
-// contactIcons[1].addEventListener("mouseenter", () => {
-//   contactIcons[1].style.transition = "color 0.5s 0s ease";
-//   contactIcons[1].style.color = "#23a8e9";
-// });
-// contactIcons[2].addEventListener("mouseenter", () => {
-//   contactIcons[2].style.transition = "color 0.5s 0s ease";
-//   contactIcons[2].style.color = "#0866ff";
-// });
-// contactIcons[3].addEventListener("mouseenter", () => {
-//   contactIcons[3].style.transition = "color 0.5s 0s ease";
-//   contactIcons[3].style.color = "#ffdd00";
-// });
-// contactIcons[0].addEventListener("mouseleave", () => {
-//   contactIcons[0].style.transition = "color 0.5s 0s ease";
-//   contactIcons[0].style.color = "#FFF";
-// });
-// contactIcons[1].addEventListener("mouseleave", () => {
-//   contactIcons[1].style.transition = "color 0.5s 0s ease";
-//   contactIcons[1].style.color = "#FFF";
-// });
-// contactIcons[2].addEventListener("mouseleave", () => {
-//   contactIcons[2].style.transition = "color 0.5s 0s ease";
-//   contactIcons[2].style.color = "#FFF";
-// });
-// contactIcons[3].addEventListener("mouseleave", () => {
-//   contactIcons[3].style.transition = "color 0.5s 0s ease";
-//   contactIcons[3].style.color = "#FFF";
-// });
