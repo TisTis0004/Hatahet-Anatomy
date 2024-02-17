25e94eb3563938a91e73408dcf37e6ece51ce7bc
Fares <fareshatahet491@gmail.com>
2024-02-17 16:30:27 +0300

    return

--- dropdown.js
+++ dropdown.js
@@ -1,8 +1,5 @@
 const bars = document.querySelector(".bars");
 const dropdown = document.querySelector(".dropdown-bars");
-bars.addEventListener("mouseover", () => {
-  dropdown.classList.add("open");
-});
-bars.addEventListener("mouseout", () => {
-  dropdown.classList.remove("open");
+bars.addEventListener("click", () => {
+  dropdown.classList.toggle("open");
 });
